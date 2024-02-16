2cca48c3d3c82bddca7c244a3b6de3a5080191bf
aroyerelafosse <alix.royere-lafosse@soprasteria.com>
2024-02-16 23:57:04 +0100

    refactor: location service notifies of location updates

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -1,33 +1,64 @@
-import { Injectable } from '@angular/core';
-import {WeatherService} from "./weather.service";
+import {Injectable, Signal, signal, WritableSignal} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
 
-export const LOCATIONS : string = "locations";
+export const LOCATIONS = 'locations';
 
 @Injectable()
 export class LocationService {
 
-  locations : string[] = [];
+    private locations: WritableSignal<Set<string>> = signal(new Set());
+    private removedLocation: Subject<string> = new Subject<string>();
+    private addedLocation: Subject<string> = new Subject<string>();
 
-  constructor(private weatherService : WeatherService) {
-    let locString = localStorage.getItem(LOCATIONS);
-    if (locString)
-      this.locations = JSON.parse(locString);
-    for (let loc of this.locations)
-      this.weatherService.addCurrentConditions(loc);
-  }
-
-  addLocation(zipcode : string) {
-    this.locations.push(zipcode);
-    localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-    this.weatherService.addCurrentConditions(zipcode);
-  }
-
-  removeLocation(zipcode : string) {
-    let index = this.locations.indexOf(zipcode);
-    if (index !== -1){
-      this.locations.splice(index, 1);
-      localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-      this.weatherService.removeCurrentConditions(zipcode);
+    constructor() {
+        const locString = localStorage.getItem(LOCATIONS);
+        if (locString) {
+            const locArray: string[] = JSON.parse(locString);
+            this.locations.set(new Set(locArray));
+        }
     }
-  }
+
+    getLocations(): Signal<Set<string>> {
+        return this.locations.asReadonly();
+    }
+
+    getAddedLocation(): Observable<string> {
+        // not using signal as if we add and remove many times the same zip code, we wouldn't be notified
+        return this.addedLocation.asObservable();
+    }
+
+    getRemovedLocation(): Observable<string> {
+        // not using signal as if we add and remove many times the same zip code, we wouldn't be notified
+        return this.removedLocation.asObservable();
+    }
+
+    addLocation(zipcode: string) {
+        const previousSize = this.locations().size;
+        this.locations.update(locations => {
+            locations.add(zipcode);
+            return locations;
+        });
+        const newSize = this.locations().size;
+        if (previousSize !== newSize) {
+            this.updateCache();
+            this.addedLocation.next(zipcode);
+        }
+    }
+
+    removeLocation(zipcode: string) {
+        const previousSize = this.locations().size;
+        this.locations.update(locations => {
+            locations.delete(zipcode);
+            return locations
+        });
+        const newSize = this.locations().size;
+        if (previousSize !== newSize) {
+            this.updateCache();
+            this.removedLocation.next(zipcode);
+        }
+    }
+
+    private updateCache(): void {
+        localStorage.setItem(LOCATIONS, JSON.stringify(Array.from(this.locations())));
+    }
 }
